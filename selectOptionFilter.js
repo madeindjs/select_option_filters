e126c29c5ab65ae1926d8de597d07ba5092ae36d
madeindjs <madeindjs@gmail.com>
2016-08-23 14:58:19 +0200

    create first select tag filled with first words

--- selectOptionFilter.js
+++ selectOptionFilter.js
@@ -39,8 +39,33 @@ function add_first_word_filter(select_id){
 	// get the select tag
 	select_tag = document.getElementById(select_id);
 
-	// create a simple input and insert just before the select tag
+	// get values of this select tag, group them and push them into `words`
+	var options = select_tag.options;
+	var words = [];
+	for (var i = 0; i < options.length; i++) {
+		// get the first word of the option text
+		var option = options[i];
+		var optionTextWords = option.text.split(" ");
+		var optionTextFirstWord = optionTextWords[0];
+		// push it in the array if not exists
+		if( words.indexOf(optionTextFirstWord) == -1 ){
+			words.push(optionTextFirstWord);
+		}
+	}
+
+	// create a select tag
 	var searchInput = document.createElement('select');
 	select_tag.parentElement.insertBefore(searchInput, select_tag);
+
+	// insert words as option in searchInput select
+	for (var i = 0; i < words.length; i++) {
+		var option = document.createElement('option');
+		option.textContent = words[i];
+		option.value = words[i];
+		searchInput.appendChild(option);
+		console.log(words[i]);
+	}
+
+	
 
 }
